c2921eb6abcce5ce917c06f43f0e6a79078bf98b
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-04-07 20:25:42 -0300

    Commit inicial

---
+++ src/main/java/br/edu/facthus/poo/Main.java
@@ -0,0 +1,18 @@
+package br.edu.facthus.poo;
+
+public class Main {
+
+	public static void exercicioGrupo0() {
+		System.out.println("Fulano de Tal");
+		System.out.println("Cicrano de Tal");
+	}
+
+	// TODO: acrescentar os outros grupos
+
+	public static void main(String[] args) {
+		System.out.println("Projeto POO - 2021/01");
+
+		exercicioGrupo0();
+	}
+
+}
