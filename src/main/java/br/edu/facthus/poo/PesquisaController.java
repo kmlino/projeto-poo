4b44aa11e1962c4454471ef1cff9f66650379454
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-05-21 22:17:28 -0300

    Cria os itens de teste

--- src/main/java/br/edu/facthus/poo/PesquisaController.java
+++ src/main/java/br/edu/facthus/poo/PesquisaController.java
@@ -4,20 +4,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-
 public class PesquisaController {
 	
-	// Ex. 03b - Completar...
 	@FXML
 	private TextField txtNumero;
-	
 	
 	@FXML
 	private Label lblSaida;
 	
 	@FXML
 	private void pesquisa() {
-		// Imprimir no console "Pesquisando produto com código ...
+		// EX06: completar...
 		Integer n1 = Integer.parseInt(txtNumero.getText());
 		
 		lblSaida.setText(String.format("Pesquisando produto com código..."
@@ -26,7 +23,7 @@ public class PesquisaController {
 	
 	@FXML
 	private void entrada() {
-		// Imprimir no console "Registrando entrada de ... itens"
+		// EX07: completar...
 		Integer n1 = Integer.parseInt(txtNumero.getText());
 		
 		lblSaida.setText(String.format("Pesquisando entrada de...itens"
@@ -35,7 +32,7 @@ public class PesquisaController {
 	
 	@FXML
 	private void saida() {
-		// Imprimir no console "Registrando saíde de ... itens"
+		// EX08: completar...
 		Integer n1 = Integer.parseInt(txtNumero.getText());
 		
 		lblSaida.setText(String.format("Pesquisando saída de...itens"
